03a531ece0bb8cd32060923bff5c516a1cc72e62
rimgosu <newnyup@gmail.com>
2023-12-02 18:34:46 +0900

    [feat] dot update

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.tools.SassCompiler;
  
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +12,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
 
 }
